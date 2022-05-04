e648577150444a81325a785ebf46e5fc87c15295
BladeKitten <AlbedoX@DESKTOP-3U8UV8O.fritz.box>
2022-05-05 00:39:42 +0200

    Minimal changes

--- src/main/java/net/mcreator/caityschanges/init/CaitysChangesModItems.java
+++ src/main/java/net/mcreator/caityschanges/init/CaitysChangesModItems.java
@@ -26,9 +26,9 @@ import java.util.ArrayList;
 @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
 public class CaitysChangesModItems {
 	private static final List<Item> REGISTRY = new ArrayList<>();
+	public static final Item EMERALDIOTOOLS_SWORD = register(new EmeraldiotoolsSwordItem());
 	public static final Item EMERALDIOTOOLS_PICKAXE = register(new EmeraldiotoolsPickaxeItem());
 	public static final Item EMERALDIOTOOLS_AXE = register(new EmeraldiotoolsAxeItem());
-	public static final Item EMERALDIOTOOLS_SWORD = register(new EmeraldiotoolsSwordItem());
 	public static final Item EMERALDIOTOOLS_SHOVEL = register(new EmeraldiotoolsShovelItem());
 	public static final Item EMERALDIOTOOLS_HOE = register(new EmeraldiotoolsHoeItem());
 	public static final Item TEMPERED_EMERALD = register(new TemperedEmeraldItem());
